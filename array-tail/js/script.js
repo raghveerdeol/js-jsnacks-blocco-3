f8d3121e47d6de6f5faff56acd98409b03fe1b0d
raghveerdeol <deolraghveer5@gmail.com>
2024-05-14 22:05:21 +0200

    aggiunto 2 div attraverso js con il rispettivo contenuto

--- array-tail/js/script.js
+++ array-tail/js/script.js
@@ -9,10 +9,20 @@
 const gina = [];
 const n = Number.parseInt(prompt('inserisci il numero degli elementi di gina'));
 const x = Number.parseInt(prompt('inserisci il numero degli elementi da mostrare'));
+const main = document.querySelector("main");
+const corpo = document.createElement("div");
+const coda = document.createElement("div");
+corpo.classList.add("corpo");
+coda.classList.add("ugas");
+main.appendChild(corpo);
+main.appendChild(coda);
 
 for (let index = 0; index < n; index++) {
+
     gina.push(Math.floor(Math.random() * 100) + 1)
 }
 const uga = gina.slice(n - x, n);
+corpo.append('Questo è il corpo di gina:' + ' ' + gina);
+coda.append('Questa è la coda di gina:' + ' ' + uga);
 
-console.log(gina, n, uga)
+console.log(gina, n, uga);

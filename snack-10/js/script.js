3918e1eb2577558665051f6ec7f26e50c31d7ff6
raghveerdeol <deolraghveer5@gmail.com>
2024-05-15 12:26:33 +0200

    aggiunto while con smallarray.push

--- snack-10/js/script.js
+++ snack-10/js/script.js
@@ -4,14 +4,21 @@ Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti qu
 const luffy = [1,2,3,4,];
 const zoro = [1,2,3,4,5,6,7,8,9,];
 let bigarray = 0;
+let smallarray = 0;
 
 if (luffy.length === zoro.length) {
     console.log('Sono della stessa lunghezza.')
 } else if(luffy > zoro){
     bigarray = luffy;
+    smallarray = zoro;
 } else{
     bigarray = zoro;
+    smallarray = luffy;
 }
 
+while (smallarray.length < bigarray.length) {
+    smallarray.push((Math.floor(Math.random() * 100) + 1));
+    
+}
 
-console.log(zoro, luffy, bigarray)
+console.log(zoro, luffy)

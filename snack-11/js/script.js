52a235114b73bea9219adac4807b36420fdf24ec
raghveerdeol <deolraghveer5@gmail.com>
2024-05-15 12:51:10 +0200

    creato white somma < 50 con somma = somma + x

---
+++ snack-11/js/script.js
@@ -0,0 +1,15 @@
+/* Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
+Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50. */
+
+const lino = [];
+const z = Number.parseInt(prompt('inserire un numero'));
+lino.push(z);
+let somma = z;
+
+while (somma < 50) {
+    const x = Number.parseInt(prompt('inserire un numero'));
+    lino.push(x)
+    somma = somma + x;
+}
+
+console.log(lino)

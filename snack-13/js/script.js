c65e2c92d715f7b43fdbe7f50973510e3d5eb45f
raghveerdeol <deolraghveer5@gmail.com>
2024-05-16 12:37:44 +0200

    correzione esercizio

--- snack-13/js/script.js
+++ snack-13/js/script.js
@@ -1,6 +1,6 @@
 // > Scrivi una funzione che dato un numero inverta le sue cifre: 1234 -> 4321
 
-function inverti(numero) {
+/* function inverti(numero) {
     return (
         parseFloat(
         numero
@@ -12,6 +12,18 @@ function inverti(numero) {
     )
     
 }
+ */
+function inverti(numero) {
+    const string = numero + '';
+    let reversedString = '';
+    
+    for (let index = string.length - 1; index >= 0; index--) {
+        reversedString += string[index];
+        
+    }
+    return Number.parseInt(reversedString, 10);
+}
+
 let num = 321;
 inverti(num);
-console.log(inverti(num))
+console.log(inverti(num));

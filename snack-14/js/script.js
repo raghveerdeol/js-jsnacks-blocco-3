92bb34465eef964e356ac94ee6e4c6f45fb0b116
raghveerdeol <deolraghveer5@gmail.com>
2024-05-21 12:36:23 +0200

    aggiunto timer al dom

--- snack-14/js/script.js
+++ snack-14/js/script.js
@@ -1,9 +1,11 @@
 // ! Chiedi all'utente un numero di secondi da attendere e fai partire un timer da quel momento a zero secondi, interrompendo il timer
 
 let userNumber = Number.parseInt(prompt("inserisci il timer"));
-
+const mainEl = document.querySelector("main");
+let hEL = document.createElement("h1");
+mainEl.appendChild(hEL);
 const timer = setInterval(function(){
-    userNumber--;
+    hEL.append(userNumber--);
     if (userNumber <= 0) {
         clearInterval(timer);
     }
